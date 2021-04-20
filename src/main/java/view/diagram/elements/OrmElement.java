e54dc591802cabfc110d84298d4a394ad7a5d1be
2Kalnov <jdmcorolla21@gmail.com>
2021-04-20 11:59:03 +0300

    Basic ORM element interface

---
+++ src/main/java/view/diagram/elements/OrmElement.java
@@ -0,0 +1,11 @@
+package view.diagram.elements;
+
+import org.netbeans.api.visual.action.WidgetAction;
+import org.netbeans.api.visual.widget.Widget;
+import view.diagram.elements.graphics.shapes.ShapeStrategy;
+
+import java.util.List;
+
+public interface OrmElement {
+  ElementType getType();
+}
