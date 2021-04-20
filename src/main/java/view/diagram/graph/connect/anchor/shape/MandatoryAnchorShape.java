57968c2498968618b6b257df472f126afd2bee5e
2Kalnov <jdmcorolla21@gmail.com>
2021-04-20 11:52:02 +0300

    Mandatory anchor shape class

---
+++ src/main/java/view/diagram/graph/connect/anchor/shape/MandatoryAnchorShape.java
@@ -0,0 +1,35 @@
+package view.diagram.graph.connect.anchor.shape;
+
+import org.netbeans.api.visual.anchor.AnchorShape;
+import view.diagram.elements.colors.OrmColorFactory;
+
+import java.awt.*;
+
+class MandatoryAnchorShape implements AnchorShape {
+  private static int RADIUS = 10;
+
+  @Override
+  public boolean isLineOriented() {
+    return false;
+  }
+
+  @Override
+  public int getRadius() {
+    return RADIUS;
+  }
+
+  @Override
+  public double getCutDistance() {
+    return RADIUS / 2.0;
+  }
+
+  @Override
+  public void paint(Graphics2D graphics2D, boolean source) {
+    Color prevColor = graphics2D.getColor();
+
+    graphics2D.setColor(OrmColorFactory.getPurple());
+    graphics2D.fillOval(- RADIUS / 2, - RADIUS / 2, RADIUS, RADIUS);
+
+    graphics2D.setColor(prevColor);
+  }
+}
