d418cb17c62655d199a4d5f30b5181fccf2e371c
2Kalnov <jdmcorolla21@gmail.com>
2021-04-21 12:11:39 +0300

    Role widget

---
+++ src/main/java/view/diagram/elements/Role.java
@@ -0,0 +1,93 @@
+package view.diagram.elements;
+
+import org.netbeans.api.visual.action.WidgetAction;
+import org.netbeans.api.visual.anchor.Anchor;
+import org.netbeans.api.visual.anchor.AnchorFactory;
+import org.netbeans.api.visual.laf.LookFeel;
+import org.netbeans.api.visual.layout.LayoutFactory;
+import org.netbeans.api.visual.widget.*;
+import org.netbeans.modules.visual.anchor.DirectionalAnchor;
+import org.netbeans.modules.visual.util.GeomUtil;
+import view.diagram.elements.edit.LabelEditor;
+import view.diagram.elements.graphics.SwingAbstractBox;
+import view.diagram.elements.graphics.shapes.RoleShapeStrategy;
+import view.diagram.elements.graphics.shapes.ShapeStrategy;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+public class Role extends Widget implements OrmWidget {
+
+  private final static String DEFAULT_ROLE = "<role>";
+  private final static WidgetAction EDIT_ROLE_ACTION = LabelEditor.withDefaultLabel(DEFAULT_ROLE);
+
+  private final Widget roleBox;
+  private final LabelWidget roleLabel;
+  private final OrmElement element;
+
+  public Role(OrmElement element, Scene scene) {
+    super(scene);
+
+    this.element = element;
+    LookFeel lookFeel = scene.getLookFeel();
+    setLayout(LayoutFactory.createVerticalFlowLayout(LayoutFactory.SerialAlignment.CENTER, lookFeel.getMargin()));
+
+    this.roleBox = new RoleBox(scene);
+
+    this.roleLabel = new LabelWidget(scene, DEFAULT_ROLE);
+    roleLabel.setAlignment(LabelWidget.Alignment.CENTER);
+    this.roleLabel.getActions().addAction(EDIT_ROLE_ACTION);
+
+    addChild(this.roleBox);
+    addChild(this.roleLabel);
+
+    roleLabel.addDependency((Dependency)this.roleBox);
+  }
+
+  public static class RoleBox extends ComponentWidget implements Dependency {
+    final static ShapeStrategy SHAPE = new RoleShapeStrategy();
+
+    public RoleBox(Scene scene) {
+      super(scene, new SwingAbstractBox(SHAPE));
+    }
+
+    @Override
+    public void revalidateDependency() {
+      this.revalidate();
+    }
+
+    public static ShapeStrategy getShape() {
+      return SHAPE;
+    }
+  }
+
+  @Override
+  public OrmElement getElement() {
+    return element;
+  }
+
+  @Override
+  public ShapeStrategy getShape() {
+    return RoleBox.getShape();
+  }
+
+  @Override
+  public void attachActions(List<WidgetAction> actions) {
+    WidgetAction.Chain widgetActions = this.roleBox.getActions();
+
+    for(WidgetAction action : actions) {
+      widgetActions.addAction(action);
+    }
+  }
+
+  @Override
+  public void attachAction(WidgetAction action) {
+    getActions().addAction(action);
+  }
+
+  @Override
+  public Widget getWidget() {
+    return this;
+  }
+}
