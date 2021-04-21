8441443ecfe45efb5946f3735797fedcee0ef9f5
2Kalnov <jdmcorolla21@gmail.com>
2021-04-21 12:11:55 +0300

    Value widget

---
+++ src/main/java/view/diagram/elements/Value.java
@@ -0,0 +1,51 @@
+package view.diagram.elements;
+
+import org.netbeans.api.visual.action.WidgetAction;
+import org.netbeans.api.visual.widget.ComponentWidget;
+import org.netbeans.api.visual.widget.Scene;
+import org.netbeans.api.visual.widget.Widget;
+import view.diagram.elements.graphics.SwingAbstractBox;
+import view.diagram.elements.graphics.shapes.ShapeStrategy;
+import view.diagram.elements.graphics.shapes.ValueShapeStrategy;
+
+import java.util.List;
+
+public class Value extends ComponentWidget implements OrmWidget {
+  private static final String DEFAULT_VALUE = "<value>";
+  private static final ShapeStrategy SHAPE = new ValueShapeStrategy();
+
+  private final OrmElement element;
+
+  public Value(OrmElement element, Scene scene) {
+    super(scene, new SwingAbstractBox(SHAPE));
+    this.element = element;
+  }
+
+  @Override
+  public ShapeStrategy getShape() {
+    return SHAPE;
+  }
+
+  @Override
+  public void attachActions(List<WidgetAction> actions) {
+    for(WidgetAction a : actions) {
+
+    }
+
+  }
+
+  @Override
+  public void attachAction(WidgetAction action) {
+
+  }
+
+  @Override
+  public OrmElement getElement() {
+    return null;
+  }
+
+  @Override
+  public Widget getWidget() {
+    return null;
+  }
+}
