f346f93ef18a762f3efdc8b8027d90e1cb13fe35
2Kalnov <jdmcorolla21@gmail.com>
2021-04-21 20:15:51 +0300

    Shape strategy factory providing shape singletons

---
+++ src/main/java/view/diagram/elements/graphics/shapes/ShapeStrategyFactory.java
@@ -0,0 +1,24 @@
+package view.diagram.elements.graphics.shapes;
+
+public class ShapeStrategyFactory {
+  private static final ShapeStrategy ENTITY = new EntityShapeStrategy();
+  private static final ShapeStrategy VALUE = new ValueShapeStrategy();
+  private static final ShapeStrategy ROLE = new RoleShapeStrategy();
+  private static final ShapeStrategy BINARY_PREDICATE = new BinaryPredicateShapeStrategy();
+
+  public static ShapeStrategy entity() {
+    return ENTITY;
+  }
+
+  public static ShapeStrategy value() {
+    return VALUE;
+  }
+
+  public static ShapeStrategy role() {
+    return ROLE;
+  }
+
+  public static ShapeStrategy binaryPredicate() {
+    return BINARY_PREDICATE;
+  }
+}
