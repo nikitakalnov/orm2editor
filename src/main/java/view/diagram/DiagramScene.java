dcf509578d96686669fcc103c439810b159f2457
2Kalnov <jdmcorolla21@gmail.com>
2021-04-21 12:35:03 +0300

    Main window

---
+++ src/main/java/view/diagram/DiagramScene.java
@@ -0,0 +1,47 @@
+package view.diagram;
+
+import org.netbeans.api.visual.graph.layout.GraphLayoutFactory;
+import org.netbeans.api.visual.layout.LayoutFactory;
+import org.netbeans.api.visual.layout.SceneLayout;
+import view.diagram.elements.ElementType;
+import view.diagram.elements.Entity;
+import view.diagram.elements.Role;
+import view.diagram.graph.Graph;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DiagramScene extends JFrame {
+  public DiagramScene() {
+    setMinimumSize(new Dimension(800, 400));
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    JPanel contentPane = new JPanel(new BorderLayout());
+
+    setContentPane(contentPane);
+
+    initComponents(contentPane);
+
+    setTitle("NetBeans Visual Library example");
+    pack();
+    setVisible(true);
+  }
+
+  private void initComponents(JPanel contentPane) {
+    JScrollPane workspace = new JScrollPane();
+    contentPane.add(workspace, BorderLayout.CENTER);
+
+    Graph graph = new Graph();
+
+    graph.addNode(() -> ElementType.ENTITY);
+    graph.addNode(() -> ElementType.ENTITY);
+    graph.addNode(() -> ElementType.ROLE);
+
+    SceneLayout graphLayout = LayoutFactory.createSceneGraphLayout(graph, GraphLayoutFactory.createHierarchicalGraphLayout(graph, true));
+    graphLayout.invokeLayoutImmediately();
+
+    workspace.setViewportView(graph.createView());
+
+    contentPane.add(graph.createSatelliteView(), BorderLayout.WEST);
+  }
+}
