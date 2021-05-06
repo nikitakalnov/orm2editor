0a006ee36b0a3f2eab840ad289c43c4012444263
2Kalnov <jdmcorolla21@gmail.com>
2021-05-06 12:18:00 +0300

    Added constraints types

--- src/main/java/view/diagram/elements/core/ElementType.java
+++ src/main/java/view/diagram/elements/core/ElementType.java
@@ -1,10 +1,26 @@
 package view.diagram.elements.core;
 
 public enum ElementType {
-  ENTITY,
-  VALUE,
-  ROLE,
-  CONSTRAINT,
-  BINARY_PREDICATE,
-  SUBTYPING
+  ENTITY(true),
+  VALUE(true),
+  ROLE(true),
+  BINARY_PREDICATE(true),
+
+  EQUALITY_CONSTRAINT(false),
+  SUBSET_CONSTRAINT(false),
+  XOR_CONSTRAINT(false),
+  EXCLUSION_CONSTRAINT(false),
+
+  SUBTYPING(false);
+
+  // ORM Element can be either an object or a constraint (including subtyping constraint)
+  protected final boolean isObject;
+
+  ElementType(boolean isObject) {
+    this.isObject = isObject;
+  }
+
+  public boolean isObject() {
+    return isObject;
+  }
 }
