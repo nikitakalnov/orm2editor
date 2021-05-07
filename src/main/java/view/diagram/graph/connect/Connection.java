350420dc4375cce4e10afb7376edff75401042a9
2Kalnov <jdmcorolla21@gmail.com>
2021-05-07 23:14:50 +0300

    Added widget and connect providers to Connection class

--- src/main/java/view/diagram/graph/connect/Connection.java
+++ src/main/java/view/diagram/graph/connect/Connection.java
@@ -1,15 +1,28 @@
 package view.diagram.graph.connect;
 
+import org.netbeans.api.visual.widget.ConnectionWidget;
 import view.diagram.elements.core.OrmElement;
+import view.diagram.graph.connect.providers.OrmConnectProvider;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 // TODO: make class abstract
 public class Connection {
   private OrmElement source;
   private OrmElement target;
+  private final ConnectionWidget widget;
 
-  public Connection(OrmElement source, OrmElement target) {
+  /**
+   * Для дуг, которые можно соединять друг с другом
+   */
+  private List<OrmConnectProvider> connectProviders = new ArrayList<>();
+
+  public Connection(OrmElement source, OrmElement target, ConnectionWidget widget) {
     this.source = source;
     this.target = target;
+    this.widget = widget;
   }
 
   public void setSource(OrmElement source) {
@@ -26,5 +39,17 @@ public class Connection {
 
   public OrmElement getTarget() {
     return target;
+  }
+
+  public ConnectionWidget getWidget() {
+    return widget;
+  }
+
+  public List<OrmConnectProvider> getConnectProviders() {
+    return Collections.unmodifiableList(connectProviders);
+  }
+
+  public void addConnectProvider(OrmConnectProvider provider) {
+    this.connectProviders.add(provider);
   }
 }
