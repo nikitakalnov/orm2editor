65a3076995f6a0a33a0bd2b7dcc563260862ab49
2Kalnov <jdmcorolla21@gmail.com>
2021-05-13 16:54:31 +0300

    Added removeAction method with default implementation

--- src/main/java/view/diagram/elements/core/OrmWidget.java
+++ src/main/java/view/diagram/elements/core/OrmWidget.java
@@ -2,10 +2,8 @@ package view.diagram.elements.core;
 
 import org.netbeans.api.visual.action.WidgetAction;
 import org.netbeans.api.visual.widget.Widget;
-import org.netbeans.modules.visual.action.ConnectAction;
 
 import java.awt.*;
-import java.util.List;
 
 public interface OrmWidget {
   Dimension getSize();
@@ -13,6 +11,9 @@ public interface OrmWidget {
     attachAction(action);
   }
   void attachAction(WidgetAction action);
+  default void removeAction(WidgetAction action) {
+    getWidget().getActions().removeAction(action);
+  }
   OrmElement getElement();
   Widget getWidget();
 }
