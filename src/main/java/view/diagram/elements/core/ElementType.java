e8d8b1bb761085884637d61efedc34bdf7e1a9d7
2Kalnov <jdmcorolla21@gmail.com>
2021-05-28 23:06:42 +0300

    ElementType contains corresponding DiagramElement class

--- src/main/java/view/diagram/elements/core/ElementType.java
+++ src/main/java/view/diagram/elements/core/ElementType.java
@@ -1,26 +1,34 @@
 package view.diagram.elements.core;
 
+import org.vstu.nodelinkdiagram.DiagramElement;
+import org.vstu.orm2diagram.model.ORM_EntityType;
+import org.vstu.orm2diagram.model.ORM_Predicate;
+import org.vstu.orm2diagram.model.ORM_Subtyping;
+import org.vstu.orm2diagram.model.ORM_ValueType;
+
 import java.util.LinkedList;
 import java.util.List;
 
 public enum ElementType {
-  ENTITY(ElementCategory.OBJECT),
-  VALUE(ElementCategory.OBJECT),
-  UNARY_PREDICATE(ElementCategory.OBJECT),
-  BINARY_PREDICATE(ElementCategory.OBJECT),
+  ENTITY(ElementCategory.OBJECT, ORM_EntityType.class),
+  VALUE(ElementCategory.OBJECT, ORM_ValueType.class),
+  UNARY_PREDICATE(ElementCategory.OBJECT, null),
+  BINARY_PREDICATE(ElementCategory.OBJECT, ORM_Predicate.class),
 
-  EQUALITY_CONSTRAINT(ElementCategory.SET_COMPARISON_CONSTRAINT),
-  SUBSET_CONSTRAINT(ElementCategory.SET_COMPARISON_CONSTRAINT),
-  XOR_CONSTRAINT(ElementCategory.SET_COMPARISON_CONSTRAINT),
-  EXCLUSION_CONSTRAINT(ElementCategory.SET_COMPARISON_CONSTRAINT),
+  EQUALITY_CONSTRAINT(ElementCategory.SET_COMPARISON_CONSTRAINT, null),
+  SUBSET_CONSTRAINT(ElementCategory.SET_COMPARISON_CONSTRAINT, null),
+  XOR_CONSTRAINT(ElementCategory.SET_COMPARISON_CONSTRAINT, null),
+  EXCLUSION_CONSTRAINT(ElementCategory.SET_COMPARISON_CONSTRAINT, null),
 
-  SUBTYPING(ElementCategory.SUBTYPING_CONSTRAINT);
+  SUBTYPING(ElementCategory.SUBTYPING_CONSTRAINT, ORM_Subtyping.class);
 
   // ORM Element can be either an object or a constraint (including subtyping constraint)
   protected final ElementCategory category;
+  protected final Class<? extends DiagramElement> elementClass;
 
-  ElementType(ElementCategory category) {
+  ElementType(ElementCategory category, Class<? extends DiagramElement> elementClass) {
     this.category = category;
+    this.elementClass = elementClass;
   }
 
   public ElementCategory getCategory() {
@@ -36,5 +44,24 @@ public enum ElementType {
         categoryTypes.add(type);
 
     return categoryTypes;
+  }
+
+  public Class<? extends DiagramElement> getElementClass() {
+    return elementClass;
+  }
+
+  public static ElementType withClass(Class<? extends DiagramElement> c) throws ElementNotExistException {
+    for(ElementType type : values()) {
+      if(type.elementClass.equals(c))
+        return type;
+    }
+
+    throw new ElementNotExistException(c);
+  }
+
+  public static class ElementNotExistException extends Exception {
+    public ElementNotExistException(Class<? extends DiagramElement> c) {
+      super(c.getCanonicalName() + " is not representing any ORM element");
+    }
   }
 }
