9f02ae0aad9d54449d42111201abd68071c13d41
2Kalnov <jdmcorolla21@gmail.com>
2021-05-28 23:09:57 +0300

    ConnectionUtils class for resolving edge class based on source and target type

---
+++ src/main/java/view/diagram/graph/connect/ConnectionUtils.java
@@ -0,0 +1,59 @@
+package view.diagram.graph.connect;
+
+import org.vstu.nodelinkdiagram.DiagramEdge;
+import org.vstu.orm2diagram.model.ORM_Subtyping;
+import view.diagram.elements.core.ElementType;
+import view.diagram.elements.core.OrmElement;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+public class ConnectionUtils {
+  static class ConnectionType {
+    private final ElementType sourceType;
+    private final ElementType targetType;
+
+    public ConnectionType(ElementType sourceType, ElementType targetType) {
+      this.sourceType = sourceType;
+      this.targetType = targetType;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if(obj instanceof ConnectionType) {
+        ConnectionType other = (ConnectionType)obj;
+
+        return other.getSourceType().equals(sourceType) && other.getTargetType().equals(targetType);
+      }
+      else
+        return false;
+    }
+
+    public ElementType getSourceType() {
+      return sourceType;
+    }
+
+    public ElementType getTargetType() {
+      return targetType;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(sourceType, targetType);
+    }
+  }
+
+  private static final Map<ConnectionType, Class<? extends DiagramEdge>> EDGE_TYPES = new HashMap<>();
+
+  static {
+    EDGE_TYPES.put(new ConnectionType(ElementType.ENTITY, ElementType.ENTITY), ORM_Subtyping.class);
+  }
+
+  public static Class<? extends DiagramEdge> getType(OrmElement source, OrmElement target) {
+    return Optional
+            .ofNullable(EDGE_TYPES.get(new ConnectionType(source.getType(), target.getType())))
+            .orElseThrow(() -> new RuntimeException("Can not connect " + source.toString() + " and " + target.toString()));
+  }
+}
