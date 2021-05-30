19b06090236390c0500391bdd4aae21c4f261e97
2Kalnov <jdmcorolla21@gmail.com>
2021-05-30 21:07:32 +0300

    Brighter ERROR color

--- src/main/java/view/diagram/colors/OrmColorFactory.java
+++ src/main/java/view/diagram/colors/OrmColorFactory.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 public class OrmColorFactory {
   private static Color PURPLE = new Color(166, 68, 173);
-  private static Color ERROR = new Color(168, 50, 50);
+  private static Color ERROR = new Color(242, 62, 62);
   private static Color SUCCESS = new Color(50, 168, 82);
   private static Color PROBLEMS = new Color(168, 168, 50);
 
