101c87fb669a2fd60d840d51f1c6975b9ad6de5b
2Kalnov <jdmcorolla21@gmail.com>
2021-05-30 13:27:09 +0300

    Added new colors

--- src/main/java/view/diagram/colors/OrmColorFactory.java
+++ src/main/java/view/diagram/colors/OrmColorFactory.java
@@ -4,12 +4,23 @@ import java.awt.*;
 
 public class OrmColorFactory {
   private static Color PURPLE = new Color(166, 68, 173);
+  private static Color ERROR = new Color(168, 50, 50);
+  private static Color SUCCESS = new Color(50, 168, 82);
+  private static Color PROBLEMS = new Color(168, 168, 50);
 
   public static Color getPurple() {
     return PURPLE;
   }
 
   public static Color getError() {
-    return Color.RED;
+    return ERROR;
+  }
+
+  public static Color getSuccess() {
+    return SUCCESS;
+  }
+
+  public static Color getProblems() {
+    return PROBLEMS;
   }
 }
