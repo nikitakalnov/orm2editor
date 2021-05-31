d80b6c7f77a86c949ab05afe07e1fe80a8fafccc
2Kalnov <jdmcorolla21@gmail.com>
2021-05-31 11:13:04 +0300

    Added getElement() to OrmConnector

--- src/main/java/view/diagram/elements/core/OrmConnector.java
+++ src/main/java/view/diagram/elements/core/OrmConnector.java
@@ -1,8 +1,11 @@
 package view.diagram.elements.core;
 
+import org.vstu.nodelinkdiagram.DiagramNode;
+
 /**
  * Widget drawn by parent OrmWidget and representing connection point
  */
 public interface OrmConnector {
   OrmWidget getParent();
+  OrmElement getElement();
 }
